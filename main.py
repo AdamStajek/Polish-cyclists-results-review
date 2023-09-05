0c3a6c8eabd1fa51d7ee45025235ec715365ef39
AdamStajek <adasta146@gmail.com>
2023-09-05 15:29:01 +0200

    Database added

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
+import psycopg2
 
+# connecting with database
+conn = psycopg2.connect("dbname = First_Cycling user=postgres password=postgres")
+conn.autocommit = True
+cursor = conn.cursor()
+cursor.execute("TRUNCATE TABLE public.\"Results\" RESTART IDENTITY")
+cursor.execute("TRUNCATE TABLE public.cyclists RESTART IDENTITY")
 
-#extract list of race links in the past 3 days
+# extract list of race links in the past 3 days
 home_html = requests.get("https://firstcycling.com/").text
 soup = BeautifulSoup(home_html, "lxml")
-date_container = soup.find("div", id="tabs-container")
-dates = date_container.find_all("div", class_="tab-content")
 race_links = []
-for date in dates:
-    race_refs = date.find_all('a', title="Results")
-    for race_ref in race_refs:
-        race_links.append(race_ref.get('href'))
+# current_day = datetime.today().strftime("%B") + str(datetime.today().day)
+current_day = "September3"
+fc_current_day = soup.find('div', id=current_day)
+race_refs = fc_current_day.find_all('a', title="Results")
+for race_ref in race_refs:
+    race_links.append(race_ref.get('href'))
 
-
+# extract polish riders from results
 for race_link in race_links:
     race_html = requests.get("https://firstcycling.com/" + race_link).text
     soup = BeautifulSoup(race_html, "lxml")
     standings = soup.find("div", class_="tab-content results")
     cyclists = standings.find_all("tr")
-    print(soup.h1.text)
     for cyclist in cyclists:
         if cyclist.find("span", class_="flag flag-pl"):
-            if cyclist.td.string == "DNF" or cyclist.td.string == "DNS":
-                print(f"{cyclist.td.string} {cyclist.a['title']}")
+            race = soup.h1.text
+            cyclist_name = cyclist.a['title']
+            rank = cyclist.td.string
+            cursor.execute(f"INSERT INTO public.cyclists VALUES (DEFAULT, \'{cyclist_name}\')")
+            if cyclist.td.string == "DNF" or rank == "DNS" or cyclist.td.string == "OOT":
+                #print(f"{race},{cyclist_name}, {rank}")
+                cursor.execute(f"INSERT INTO public.\"Results\" VALUES (DEFAULT, \'{race}\', \'{cyclist_name}\', \'{rank}\', \'NULL\')")
             else:
-                print(f"{cyclist.td.string}. {cyclist.a['title']} {cyclist.contents[-2].string.replace(' ', '')}")
+                time = cyclist.contents[-2].string.replace(' ','')
+                #print(f"{race},{cyclist_name},{rank}.,{time}")
+                cursor.execute(f"INSERT INTO public.\"Results\" VALUES (DEFAULT, \'{race}\', \'{cyclist_name}\', \'{rank}.\', \'{time}\')")
 
 
